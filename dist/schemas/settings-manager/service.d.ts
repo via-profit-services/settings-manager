4309aeed3dad3afb72d41d4b1a72ae4991eb0cfc
Vasily Novosad <delhsmail@gmail.com>
2020-05-25 12:08:12 +0500

    Build

--- dist/schemas/settings-manager/service.d.ts
+++ dist/schemas/settings-manager/service.d.ts
@@ -1,24 +1,16 @@
 import { TOutputFilter } from '@via-profit-services/core';
-import { Context } from '../../context';
-import { TSettingsCategory } from './types';
+import { ISettingsNode, ISettingsParsed, Context } from './types';
 interface IProps {
     context: Context;
-}
-export interface ISettingsNode {
-    createdAt: Date;
-    updatedAt: Date;
-    id: string;
-    owner: string;
-    group: string;
-    name: string;
-    value: any;
-    category: TSettingsCategory;
 }
 declare class SettingsService {
     props: IProps;
     constructor(props: IProps);
     getSettings(filter: Partial<TOutputFilter>): Promise<ISettingsNode[]>;
     getSettingsByIds(ids: string[]): Promise<ISettingsNode[]>;
+    static DataToPseudoId(data: ISettingsParsed): string;
+    static getDataFromPseudoId(pseudoId: string): ISettingsParsed;
+    getSettingsByPseudoIds(pseudoIds: string[]): Promise<ISettingsNode[]>;
     updateSettings(id: string, settingsField: Partial<ISettingsNode>): Promise<string>;
     createSettings(settingsField: Omit<ISettingsNode, 'createdAt' | 'updatedAt'>): Promise<string>;
     deleteSettings(id: string): Promise<string>;
