f92f9c12c348d8bccf00b810ae5eb19d42398f27
Vasily Novosad <delhsmail@gmail.com>
2021-03-16 18:49:07 +0500

    Manage migrations

--- .knex/migrations/20200502205050_settings-manager-setup.js
+++ .knex/migrations/20200502205050_settings-manager-setup.js
@@ -22,39 +22,20 @@ function up(knex) {
     return __awaiter(this, void 0, void 0, function* () {
         return knex.raw(`
     
-    drop table if exists "settings";
+    drop table if exists "settings" cascade;
     
-    drop type if exists "settingsCategory";
-
-
-    create type "settingsCategory" as  enum (
-      'general',
-      'ui',
-      'contact',
-      'currency',
-      'constraint',
-      'size',
-      'label',
-      'other'
-    );
-
-    CREATE TABLE "settings" (
+    create table "settings" (
       "id" uuid NOT NULL,
       "createdAt" timestamptz NOT NULL DEFAULT now(),
       "updatedAt" timestamptz NOT NULL DEFAULT now(),
-      "group" varchar(50) NOT NULL,
-      "category" "settingsCategory" NOT NULL DEFAULT 'other'::"settingsCategory",
+      "category" varchar(100) NOT NULL,
       "owner" uuid NULL,
       "name" varchar(100) NOT NULL,
       "value" jsonb NOT NULL DEFAULT '{}'::jsonb,
       "comment" text NULL,
-      CONSTRAINT settings_pk PRIMARY KEY (id),
-      CONSTRAINT settings_un UNIQUE ("group", "category", "name", "owner")
+      CONSTRAINT settings_pk PRIMARY KEY ("id"),
+      CONSTRAINT settings_un UNIQUE ("category", "name", "owner")
     );
-    
-    comment on column "settings"."group" is 'Usually, the module name is used as the group name';
-    comment on column "settings"."category" is 'Just the name of the settings category, for example, for a catalog - items;category;orders';
-    comment on column "settings"."comment" is 'Just comment of this settings field for internal usage';
   `);
     });
 }
@@ -62,8 +43,7 @@ exports.up = up;
 function down(knex) {
     return __awaiter(this, void 0, void 0, function* () {
         return knex.raw(`
-    drop table if exists "settings";
-    drop type if exists "settingsCategory";
+    drop table if exists "settings" cascade;
   `);
     });
 }
